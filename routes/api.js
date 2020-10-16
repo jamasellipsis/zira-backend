5d402bd6a8c847a22fea2fa5732718131a70fc6a
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-10-16 14:36:26 -0500

    [Add][change name route]

--- routes/api.js
+++ routes/api.js
@@ -6,9 +6,9 @@ const apiUserClassesRoutes = require('./api/user_classes');
 const apiRoleUsersRoutes = require('./api/role_users');
 
 router.use('/users', apiUsersRoutes);
-router.use('/class', apiClassRoutes);
-router.use('/userClasses', apiUserClassesRoutes);
-router.use('/roleUsers', apiRoleUsersRoutes);
+router.use('/classes', apiClassRoutes);
+router.use('/user_class', apiUserClassesRoutes);
+router.use('/role_user', apiRoleUsersRoutes);
 
 
 module.exports = router;
