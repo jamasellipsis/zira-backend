595409b6fb995fb023aa0772ecd93fbdb45c8c78
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-10-16 15:23:06 -0500

    [change][name tables]

--- models/class.js
+++ models/class.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, type) => {
-    const Class = sequelize.define("Class", {
+    const Class = sequelize.define("class", {
         id: {
             type: type.INTEGER,
             primaryKey: true,
--- models/role.js
+++ models/role.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, type) => {
-    const Role = sequelize.define("Role", {
+    const Role = sequelize.define("role", {
         id: {
             type: type.INTEGER,
             primaryKey: true,
